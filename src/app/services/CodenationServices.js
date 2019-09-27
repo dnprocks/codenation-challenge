3711a03a1bcafdad6eeddb1db3eea59e6033e36e
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2019-09-27 03:30:32 -0300

    post subimit solution

--- src/app/services/CodenationServices.js
+++ src/app/services/CodenationServices.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const axios = require("axios");
+const FormData = require("form-data");
+const form = new FormData();
+const path = require("path");
+const fs = require("fs");
 
 class CodenationServices {
-  async getCypher() {
+  async generateData() {
     const res = await axios.get(
       process.env.CODENATION_API_URL + "generate-data",
       {
@@ -10,6 +14,37 @@ class CodenationServices {
       }
     );
     return res.data;
+  }
+
+  async submitSolution() {
+    const filePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "output",
+      "answer.json"
+    );
+    const readStream = fs.createReadStream(filePath);
+    form.append("answer", readStream);
+    const formHeaders = form.getHeaders();
+
+    try {
+      const res = await axios.post(
+        process.env.CODENATION_API_URL + "submit-solution",
+        form,
+        {
+          headers: {
+            ...formHeaders,
+            "Content-Type": "multipart/form-data"
+          },
+          params: { token: process.env.CODENATION_TOKEN }
+        }
+      );
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const sha1 = require("sha1");
 const CodenationService = require("./app/services/CodenationServices");
+const CesarCipherService = require("./app/services/CesarCipherService");
 
 setTimeout(async function() {
   try {
-    const anwser = await CodenationService.getCypher();
-    console.log(anwser.cifrado);
+    const anwser = await CodenationService.generateData();
+    const cipherDecoded = CesarCipherService.decode(
+      anwser.cifrado,
+      anwser.numero_casas
+    );
+    anwser.decifrado = cipherDecoded;
+    anwser.resumo_criptografico = sha1(cipherDecoded);
     fs.writeFile(
       path.resolve(__dirname, "..", "output", "answer.json"),
       JSON.stringify(anwser),
@@ -16,7 +23,9 @@ setTimeout(async function() {
         }
       }
     );
+    const score = await CodenationService.submitSolution();
+    console.log(score);
   } catch (error) {
-    console.log(err);
+    console.log(error);
   }
 }, 0);
